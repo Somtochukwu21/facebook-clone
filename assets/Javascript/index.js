60b20dea0e603b7bb57a02f40f651768aefd1998
Somtochukwu <you@example.com>
2022-09-16 17:17:49 +0000

    Work in progress

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,12 +1,18 @@
 let logoEl = document.getElementById("logo");
+
 let searchEl = document.getElementById("search-icon");
+
 let iconEl = document.getElementById("icon");
-iconEl.style.background = "#4496F9";
+
+iconEl.style.background = "#046EE4";
+
 logoEl.innerHTML = '<i id="logo" class="fa-brands fa-facebook-f"></i>';
-logoEl.style.fontSize = "30px";
-// let iconSearchWrapper = document.getElementById("icon-search");
+
+logoEl.style.fontSize = "32px";
+
 let recentSearches = document.getElementById("recentSearches");
-// recentSearches.style.display = "none";
+
+recentSearches.style.display = "none";
 
 function getRecentSearches() {
   searchEl.style.display = "none";
@@ -14,14 +20,19 @@ function getRecentSearches() {
   logoEl.style.fontSize = "20px";
   iconEl.style.background = "none";
   recentSearches.style.display = "flex";
-  //   iconSearchWrapper.style.background = "#242526";
 }
 
 function cancel() {
   searchEl.style.display = "flex";
   logoEl.innerHTML = '<i id="logo" class="fa-brands fa-facebook-f"></i>';
-  logoEl.style.fontSize = "30px";
+  logoEl.style.fontSize = "32px";
   iconEl.style.background = "none";
   recentSearches.style.display = "none";
-  iconEl.style.background = "#4496F9";
+  iconEl.style.background = "#046EE4";
+}
+
+function deleteUserName() {
+  const deletedUser = document.getElementById("deleteUser");
+  // alert(deletedUser)
+  deletedUser.style.display='none';
 }
